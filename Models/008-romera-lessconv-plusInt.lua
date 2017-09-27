868934ba5148a8d52830929c9c7d6815d7f84724
Egor Burkov <shrubb@ya.ru>
2017-09-27 17:20:23 +0300

    Update template (single place to set modelName)

--- Models/008-romera-lessconv-plusInt.lua
+++ Models/008-romera-lessconv-plusInt.lua
@@ -5,6 +5,7 @@ assert(nClasses)
 
 require 'nn'
 require 'cudnn'
+require 'IntegralSmartNorm'
 
 local SpatialConvolution = cudnn.SpatialConvolution
 local SpatialDilatedConvolution = nn.SpatialDilatedConvolution
@@ -15,23 +16,23 @@ local SpatialMaxPooling = cudnn.SpatialMaxPooling
 
 collectgarbage()
 
-local function downsampler(inChannels, outChannels, dropoutProb, noBN)
+local function downsampler(inChannels, outChannels, dropoutProb)
     dropoutProb = dropoutProb or 0
 
     return nn.Sequential()
-        :add(noBN and nn.Identity() or SpatialBatchNormalization(inChannels))
-        :add(noBN and nn.Identity() or ReLU(true))
-        :add((noBN or dropoutProb == 0) and nn.Identity() or nn.SpatialDropout(dropoutProb))
         :add(nn.Concat(2)
             :add(SpatialMaxPooling(2,2, 2,2, 0,0))
             :add(SpatialConvolution(inChannels, outChannels-inChannels, 3,3, 2,2, 1,1)))
+        :add(SpatialBatchNormalization(outChannels))
+        :add(dropoutProb ~= 0 and nn.SpatialDropout(dropoutProb) or nn.Identity())
+        :add(ReLU(true))
 end
 
-local function upsampler(inChannels, outChannels, noBN)
+local function upsampler(inChannels, outChannels)
     return nn.Sequential()
-        :add(noBN and nn.Identity() or SpatialBatchNormalization(inChannels))
-        :add(noBN and nn.Identity() or ReLU(true))
         :add(SpatialFullConvolution(inChannels, outChannels, 3,3, 2,2, 1,1, 1,1))
+        :add(ReLU(true))
+        :add(SpatialBatchNormalization(outChannels))
 end
 
 local function nonBt1D(inChannels, dropoutProb, dilation)
@@ -39,15 +40,11 @@ local function nonBt1D(inChannels, dropoutProb, dilation)
     dropoutProb = dropoutProb or 0
 
     local convs = nn.Sequential()
-        :add(SpatialBatchNormalization(inChannels))
-        :add(ReLU(true))
         :add(SpatialConvolution(inChannels, inChannels, 3,1, 1,1, 1,0))
         :add(ReLU(true))
         :add(SpatialConvolution(inChannels, inChannels, 1,3, 1,1, 0,1))
-
         :add(SpatialBatchNormalization(inChannels))
         :add(ReLU(true))
-        :add(dropoutProb ~= 0 and nn.SpatialDropout(dropoutProb) or nn.Identity())
 
     if dilation == 1 then convs
         :add(SpatialConvolution(inChannels, inChannels, 3,1, 1,1, 1,0))
@@ -59,10 +56,43 @@ local function nonBt1D(inChannels, dropoutProb, dilation)
         :add(SpatialDilatedConvolution(inChannels, inChannels, 1,3, 1,1, 0,dilation, dilation,dilation))
     end
 
+    convs
+        :add(SpatialBatchNormalization(inChannels))
+        :add(dropoutProb ~= 0 and nn.SpatialDropout(dropoutProb) or nn.Identity())
+
     return nn.Sequential()
         :add(nn.ConcatTable()
             :add(nn.Identity())
             :add(convs))
+        :add(nn.CAddTable())
+        :add(ReLU(true))
+end
+
+local function btInt(inChannels, btChannels, nWindows, h, w, dropoutProb)
+    dropoutProb = dropoutProb or 0
+    
+    local mainBranch = nn.Sequential()
+        :add(SpatialConvolution(inChannels, btChannels, 1,1, 1,1, 0,0))
+        :add(SpatialBatchNormalization(btChannels))
+        :add(ReLU(true))
+        
+        :add(nn.Concat(2)
+            :add(nn.Sequential()
+                :add(SpatialConvolution(btChannels, btChannels, 3,1, 1,1, 1,0))
+                :add(ReLU(true))
+                :add(SpatialConvolution(btChannels, btChannels, 1,3, 1,1, 0,1)))
+            :add(IntegralSmartNorm(btChannels, nWindows, h, w)))
+        :add(SpatialBatchNormalization(btChannels*nWindows + btChannels))
+        :add(ReLU(true))
+
+        :add(SpatialConvolution(btChannels*nWindows + btChannels, inChannels, 1,1, 1,1, 0,0))
+        :add(SpatialBatchNormalization(inChannels))
+        :add(dropoutProb ~= 0 and nn.SpatialDropout(dropoutProb) or nn.Identity())
+
+    return nn.Sequential()
+        :add(nn.ConcatTable()
+            :add(nn.Identity())
+            :add(mainBranch))
         :add(nn.CAddTable())
         :add(ReLU(true))
 end
@@ -72,8 +102,11 @@ local model = nn.Sequential()
 model:add(downsampler( 3, 16, 0.0 ))
 model:add(downsampler(16, 32, 0.03))
 
--- 128x256
 model:add(nonBt1D(32, 0.03))
+model:add(btInt(32, 16, 20, h/4, w/4, 0.03))
+model:add(btInt(32, 16, 20, h/4, w/4, 0.03))
+model:add(btInt(32, 16, 20, h/4, w/4, 0.03))
+model:add(btInt(32, 16, 20, h/4, w/4, 0.03))
 
 model:add(downsampler(32, 64, 0.3))
 
@@ -83,9 +116,11 @@ model:add(downsampler(32, 64, 0.3))
 model:add(upsampler(64, 32))
 
 model:add(nonBt1D(32))
+model:add(btInt(32, 16, 16, h/4, w/4))
 
 model:add(upsampler(32, 16))
 
+model:add(nonBt1D(16))
 model:add(nonBt1D(16))
 
 model:add(SpatialFullConvolution(16, nClasses, 3,3, 2,2, 1,1, 1,1))
